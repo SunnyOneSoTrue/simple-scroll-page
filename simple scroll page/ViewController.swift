002f0627927a84fd805651e74a54f24ed58ea5d3
SunnyOneSoTrue <kakhianisandro12@gmail.com>
2022-04-11 12:17:51 +0400

    Initial Commit

---
+++ simple scroll page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple scroll page
+//
+//  Created by USER on 11.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
